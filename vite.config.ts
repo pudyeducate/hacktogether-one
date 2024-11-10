1556c09eae54c0b7446103d6d5bbcaed37a870c2
pudyeducate <satriadevops@gmail.com>
2024-11-10 14:57:48 +0700

    add vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import vercel from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [ViteYaml(), react(), tsconfigPaths()],
+  plugins: [ViteYaml(), react(), tsconfigPaths(), vercel()],
   assetsInclude: ['**/*.glb'],
 })
